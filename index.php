13126f05a312ac93f65f57cb566db0bbc6ed9cac
rezky <rezky@atmaluhur.ac.id>
2025-10-03 16:18:04 +0700

    edit: index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php echo phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Git</title>
+</head>
+<body>
+    <h2>Belar git</h2>
+</body>
+</html>
